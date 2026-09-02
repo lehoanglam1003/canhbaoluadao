d7a2416420b647072c55dd50468e0af61e2ba620
LE HOANG LAM <lehoanglam@LAM-HOANG-LE.local>
2026-09-02 11:48:00 +0700

    Update app source code to stable WebView

---
+++ app/src/main/java/com/duan/canhbaoluadao/MainActivity.java
@@ -0,0 +1,41 @@
+package com.duan.canhbaoluadao;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try {
+            webView = new WebView(this);
+            setContentView(webView);
+
+            WebSettings settings = webView.getSettings();
+            settings.setJavaScriptEnabled(true);
+            settings.setDomStorageEnabled(true);
+            settings.setAllowFileAccess(true);
+
+            webView.setWebViewClient(new WebViewClient());
+            webView.loadUrl("file:///android_asset/index.html");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
